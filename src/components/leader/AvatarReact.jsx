6fb3efdce6a5c90b69bf1dbcf0c1b2d671731c76
JCruzMi <jcruzmi08064@universidadean.edu.co>
2023-09-28 10:06:54 -0500

    aaaaa

--- src/components/leader/AvatarReact.jsx
+++ src/components/leader/AvatarReact.jsx
@@ -1,15 +1,12 @@
-import { Avatar } from "@material-tailwind/react";
-
 export default function AvatarReact({ image, name, description, index }) {
   return index == 1 ? (
     <div className="flex flex-col gap-6 w-full">
       <div className="flex items-center w-full justify-center gap-4 text-light-blue-dark">
         <div className="flex flex-col relative gap-4 items-center text-center">
-          <Avatar
+          <img
             src={image}
             alt="avatar"
-            size="xl"
-            className="bg-light-blue-dark/20"
+            className="bg-light-blue-dark/20 inline-block relative object-cover object-center rounded-full w-20 h-20"
           />
           <div className="absolute top-16">
             <div className="font-bold w-auto px-2 py-[1px] text-xs bg-yellow-300 rounded-full text-light-blue-dark">
@@ -31,11 +28,10 @@ export default function AvatarReact({ image, name, description, index }) {
         <div className="flex flex-col gap-6 w-full">
           <div className="flex items-center w-full justify-between gap-4 text-light-blue-dark">
             <div className="flex gap-4">
-              <Avatar
+              <img
                 src={image}
                 alt="avatar"
-                size="md"
-                className="bg-light-blue-dark/20"
+                className="bg-light-blue-dark/20 inline-block relative object-cover object-center rounded-full w-12 h-12"
               />
               <div>
                 <p className="font-semibold text-xl">{name}</p>
